92cb5a9e4e5f0b65d2e8b2273f22a81723555868
nirgluzman <nir.gluzman@gmail.com>
2023-03-02 00:21:48 +0100

    CSS, layout and Todo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import "./App.css";
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to -[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState(["Learn React", "Grind Leetcode"]);
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="add Todo" />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,24 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 captilize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 captilize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center`,
+};
+
+const Todo = ({ todo }) => {
+  return (
+    <li className={style.li}>
+      <div className={style.row}>
+        <input type="checkbox" />
+        <p className={style.text}>{todo}</p>
+      </div>
+      <button>{<FaRegTrashAlt />}</button>
+    </li>
+  );
+};
+
+export default Todo;
